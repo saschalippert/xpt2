e1430c311c877373ce115afbd4ebf350a405c6a2
kerner1000 <7750815@gmail.com>
2018-08-27 18:54:42 +0200

    Make FileUtil system-independent

--- src/main/java/util/FileUtil.java
+++ src/main/java/util/FileUtil.java
@@ -1,11 +1,21 @@
 package util;
 
+import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class FileUtil {
+
+    private static final String userHome = System.getProperty("user.home");
+
+    private static final String prefix = "xpt2";
+
+    private static final String pathSeparator = File.separator;
+
+    public static final String saveDirectoryString = userHome + pathSeparator + prefix + pathSeparator + "%s"
+	    + pathSeparator + "%s" + pathSeparator
+	    + "%s" + pathSeparator + "%s" + pathSeparator + "%s.json";
 
     public static Path getFile(LocalDateTime date, String instrument) {
         String year = String.format("%02d", date.getYear());
@@ -13,6 +23,6 @@ public class FileUtil {
         String day = String.format("%02d", date.getDayOfMonth());
         String hour = String.format("%02d", date.getHour());
 
-        return Paths.get(String.format("c:/temp2/%s/%s/%s/%s/%s.json", instrument, year, month, day, hour));
+	return Paths.get(String.format(saveDirectoryString, instrument, year, month, day, hour));
     }
 }
